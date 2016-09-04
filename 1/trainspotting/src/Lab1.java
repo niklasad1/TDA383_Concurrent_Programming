68e382cad223aa855934821a628efcca7b05e4fa
niklasad1 <niklasadolfsson1@gmail.com>
2016-09-04 21:15:14 +0200

    some work

--- 1/trainspotting/src/Lab1.java
+++ 1/trainspotting/src/Lab1.java
@@ -3,10 +3,13 @@ import TSim.*;
 public class Lab1 {
 
   public Lab1(Integer speed1, Integer speed2) {
+    // I assume only one thread is created
     TSimInterface tsi = TSimInterface.getInstance();
 
     try {
       tsi.setSpeed(1,speed1);
+      // add second train
+      tsi.setSpeed(2,speed2);
     }
     catch (CommandException e) {
       e.printStackTrace();    // or only e.getMessage() for the error
