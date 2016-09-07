25226b26e6d47283a04b14f01ed40dad57b1f727
niklasad1 <niklasadolfsson1@gmail.com>
2016-09-07 22:49:41 +0200

    Ugly code hard code action upon switches, need to think about semaphores now

--- 1/trainspotting/src/Train.java
+++ 1/trainspotting/src/Train.java
@@ -12,7 +12,9 @@ public class Train extends Thread
   private int direction; 
   
   // Class variables
-  private final static Semaphore s = new Semaphore(1);
+  public static final int LEFT = 0x01;
+	public static final int RIGHT = 0x02;
+  
   
   private final static Point[] switches = new Point[] {
 	  new Point(17, 7),
@@ -22,18 +24,39 @@ public class Train extends Thread
   };
   
   
-  private final HashMap<SensorEvent, Point> switchSensor = new HashMap<SensorEvent, Point>() {{
-    put(new SensorEvent(trainID, 16, 7, SensorEvent.INACTIVE), switches[0]);
-    put(new SensorEvent(trainID, 18, 7, SensorEvent.INACTIVE), switches[0]);
-    put(new SensorEvent(trainID, 16, 10, SensorEvent.INACTIVE), switches[1]);
-    put(new SensorEvent(trainID, 14, 9, SensorEvent.INACTIVE), switches[1]);
-    put(new SensorEvent(trainID, 5, 9, SensorEvent.INACTIVE), switches[2]);
-    put(new SensorEvent(trainID, 3, 9, SensorEvent.INACTIVE), switches[2]);
-    put(new SensorEvent(trainID, 2, 11, SensorEvent.INACTIVE), switches[3]);
-    put(new SensorEvent(trainID, 4, 11, SensorEvent.INACTIVE), switches[3]);
-  }};
+  /* private final HashMap<SensorEvent, Point> switchSensor = new HashMap<SensorEvent, Point>() {{ */
+  /*   put(new SensorEvent(trainID, 16, 7, SensorEvent.INACTIVE), switches[0]); */
+  /*   put(new SensorEvent(trainID, 18, 7, SensorEvent.INACTIVE), switches[0]); */
+  /*   put(new SensorEvent(trainID, 16, 10, SensorEvent.INACTIVE), switches[1]); */
+  /*   put(new SensorEvent(trainID, 14, 9, SensorEvent.INACTIVE), switches[1]); */
+  /*   put(new SensorEvent(trainID, 5, 9, SensorEvent.INACTIVE), switches[2]); */
+  /*   put(new SensorEvent(trainID, 3, 9, SensorEvent.INACTIVE), switches[2]); */
+  /*   put(new SensorEvent(trainID, 2, 11, SensorEvent.INACTIVE), switches[3]); */
+  /*   put(new SensorEvent(trainID, 4, 11, SensorEvent.INACTIVE), switches[3]); */
+  /* }}; */
 
+  private final static SensorEvent[] switchSensor = new SensorEvent[] {
+    // Switch 1
+    new SensorEvent(-1, 16, 7, SensorEvent.INACTIVE),
+    new SensorEvent(-1, 18, 7, SensorEvent.INACTIVE),    
+    new SensorEvent(-1, 17, 8, SensorEvent.INACTIVE),    
+    // Switch 2
+    new SensorEvent(-1, 16, 9, SensorEvent.INACTIVE),
+    new SensorEvent(-1, 15, 10, SensorEvent.INACTIVE),
+    new SensorEvent(-1, 14, 9, SensorEvent.INACTIVE),
+    // Switch 3
+    new SensorEvent(-1, 5, 9, SensorEvent.INACTIVE),
+    new SensorEvent(-1, 5, 10, SensorEvent.INACTIVE),
+    new SensorEvent(-1, 3, 9, SensorEvent.INACTIVE),
+    // Switch 4
+    new SensorEvent(-1, 4, 11, SensorEvent.INACTIVE),
+    new SensorEvent(-1, 3, 12, SensorEvent.INACTIVE),
+    new SensorEvent(-1, 2, 11, SensorEvent.INACTIVE),
 
+  };
+
+  private final SensorEvent stationSensorUpper = new SensorEvent(-1, 15, 5, SensorEvent.INACTIVE);
+  private final SensorEvent stationSensorLower = new SensorEvent(-1, 15, 13, SensorEvent.INACTIVE);
 
 
   // METHODS
@@ -82,20 +105,70 @@ public class Train extends Thread
       System.out.println("DIRECTION: " + this.direction);
     }
 
+    System.out.println(this.trainID + " direction: " + this.direction);
     SensorEvent sensor = tsim.getSensor(this.trainID);
     
     if (sensor.getStatus() == SensorEvent.ACTIVE) {
       
-      for (SensorEvent s : switchSensor.keySet()) {
-        System.out.println("key: " + s + " value: " + switchSensor.get(s));
-        
-        if (sensorEventEquals(s, sensor)) {
-          Point p = switchSensor.get(s);
-          tsim.setSwitch((int)p.getX(), (int)p.getY(), this.direction); 
-          break;
-        } 
-      
+      if(sensorEventEquals(sensor, stationSensorUpper) || sensorEventEquals(sensor, stationSensorLower)) {
+        tsim.setSpeed(this.trainID, 0);
       }
+      else
+      {
+        // SWITCH ONE
+        if (sensorEventEquals(sensor, switchSensor[0]) && this.direction == RIGHT) {
+          tsim.setSwitch((int)switches[0].getX(), (int)switches[0].getY(), RIGHT); 
+        }
+        
+        else if (sensorEventEquals(sensor, switchSensor[1]) && this.direction == LEFT) {
+          tsim.setSwitch((int)switches[0].getX(), (int)switches[0].getY(), LEFT); 
+        }
+        
+        else if (sensorEventEquals(sensor, switchSensor[2]) && this.direction == RIGHT) {
+          tsim.setSwitch((int)switches[0].getX(), (int)switches[0].getY(), RIGHT); 
+        }
+      
+        // SWITCH TWO
+        // can be both may need to changed later
+        else if (sensorEventEquals(sensor, switchSensor[3]) && this.direction == RIGHT) {
+          tsim.setSwitch((int)switches[1].getX(), (int)switches[1].getY(), RIGHT); 
+        }
+        
+        /* else if (sensorEventEquals(sensor, switchSensor[4]) && this.direction == LEFT) { */
+        /*   tsim.setSwitch((int)switches[1].getX(), (int)switches[1].getY(), RIGHT);  */
+        /* } */
+        
+        else if (sensorEventEquals(sensor, switchSensor[5]) && this.direction == LEFT) {
+          tsim.setSwitch((int)switches[1].getX(), (int)switches[1].getY(), RIGHT); 
+        }
+       
+        // SWITCH THREE 
+        else if (sensorEventEquals(sensor, switchSensor[6]) && this.direction == RIGHT) {
+          tsim.setSwitch((int)switches[2].getX(), (int)switches[2].getY(), LEFT); 
+        }
+        
+        else if (sensorEventEquals(sensor, switchSensor[7]) && this.direction == RIGHT) {
+          tsim.setSwitch((int)switches[2].getX(), (int)switches[2].getY(), LEFT); 
+        }
+        
+        else if (sensorEventEquals(sensor, switchSensor[8]) && this.direction == LEFT) {
+          tsim.setSwitch((int)switches[2].getX(), (int)switches[2].getY(), RIGHT); 
+        }
+      
+        // SWITCH FOUR
+        else if (sensorEventEquals(sensor, switchSensor[9]) && this.direction == LEFT) {
+          tsim.setSwitch((int)switches[3].getX(), (int)switches[3].getY(), LEFT); 
+        }
+        
+        else if (sensorEventEquals(sensor, switchSensor[10]) && this.direction == LEFT) {
+          tsim.setSwitch((int)switches[3].getX(), (int)switches[3].getY(), LEFT); 
+        }
+        
+        else if (sensorEventEquals(sensor, switchSensor[11]) && this.direction == RIGHT) {
+          tsim.setSwitch((int)switches[3].getX(), (int)switches[3].getY(), RIGHT); 
+        }
+      
+      } 
     }
-  }
+   }
 }
