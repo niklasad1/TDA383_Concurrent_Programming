7292f273c4c6dad026857b9f3c0b833f44ab1be1
William Danielsson <wildan@ed3507-19.ed3507.studat.rh66.ii.2.hb.s.cdal.chalmers.se>
2016-09-29 11:36:19 +0200

    some progress

--- Linda/ts.erl
+++ Linda/ts.erl
@@ -5,7 +5,7 @@
 
 % returns the PID of a new (empty) tuplespace.
 new() -> 
-  Pid = spawn_link(fun() -> loop() end),
+  Pid = spawn_link(fun() -> loop([], []) end),
   % may be unnecessary if we return the PID
   catch(unregister(tsserver)),
   register(tsserver, Pid),
@@ -13,18 +13,22 @@ new() ->
 
 % returns a tuple matching Pattern from tuplespace TS. Note that this operation
 % will block if there is no such tuple.
-in(TS, Pattern) ->
+in(TS, Tuple) ->
   Ref = make_ref(),
-  TS ! {self(), Ref, Pattern},
+  TS ! {take,self(), Ref, Tuple},
   receive
-    {From, Ref, "ACK"} -> io:fwrite("ACK From ~p Ref ~p~n", [From, Ref]);
-    _ -> io:fwrite("UN-EXPECTED RESPONSE~n")
+     {From, Take} -> io:fwrite("ACK From ~p Ref ~n", [From]);
+      _ -> io:fwrite("UN-EXPECTED RESPONSE~n")
   end.
 
 % puts Tuple into the tuplespace TS.
 out(TS, Tuple) ->
   Ref = make_ref(),
-  todo.
+  TS ! {put, self(), Ref, Tuple},
+  receive
+    {From, Put} -> io:fwrite("REPLY ~p~n", [From]);
+    _ -> io:fwrite("UN-EXPECTED RESPONSE~n")
+  end.
 % --------------END------------------------------------------------------------------
 
 
@@ -49,13 +53,60 @@ match(_,_) -> false.
 % --------------END------------------------------------------------------------------
  
 % --------------OWN FUNCTIONS--------------------------------------------------------
-loop() ->
-  io:fwrite("loop() ~n"),
+loop(List, Queue) ->
+  io:fwrite("List ~p ~n", [List]),
   receive
-    {From, Ref, Data} -> 
-      io:fwrite("recv: ~p ~p ~p ~n", [From, Ref, Data]),
-      From ! {self(), Ref, "ACK"},
-      loop()
-  end.
+    {put, From, Ref, Data} ->
+           NewList=tryPutData(List,Data,From,Ref),
+           reply(From, input);
+    {take, From, Ref, Data} ->
+           % if in list then remove and reply
+           % else wait and add to waiting queue   
+           NewList=tryTakeData(List,Data,From,Ref),
+           reply(From, take);
+    true -> io:format("WRONG FORMAT ~n"),
+            NewList=List
+  end,
+   loop(NewList, Queue).
 
+tryPutData(ServerData, Data,Pid, Ref) ->
+                       case find(ServerData,Data) of
+                                      not_found ->
+                                      io:format("~w added ~w to Server~n",[Pid,Data]),
+                            [{Data,Pid,Ref}|ServerData];
+                            found ->
+                                   io:format("~w failed to add ~w to Server~n",[Pid, Data]),
+                            ServerData
+                        end.
+ tryTakeData(ServerData, Data, Pid, Ref) ->
+			case find(ServerData,Data) of
+			found ->
+			io:format("~w extracted ~w from Server~n", [Pid, Data]),
+			delete(ServerData,Data);
+			not_found ->
+			io:format("~w failed to extract ~w from Server",[Pid,Data]),
+			ServerData
+		        end.
+delete([First|Rest],A) ->
+                       if
+                                First==A ->
+                                      Rest;
+                                true ->
+                                     [First|delete(Rest,A)]
+                       end;
 
+delete([],A) ->
+             [].
+
+find([First|Rest], A) ->
+                   if
+                        First == A ->
+                              found;
+                        true ->
+                             find(Rest,A)
+                        end;
+find([],A) ->
+not_found.
+
+reply(Pid, Msg) ->
+         Pid ! {self(), Msg}.
